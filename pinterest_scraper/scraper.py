1339088f0bbb4f95cf917d289c888158abc76e43
xjdeng <xjd001@gmail.com>
2018-02-28 23:19:22 -0600

    Feat: add debug parameter to runme()

--- pinterest_scraper/scraper.py
+++ pinterest_scraper/scraper.py
@@ -69,7 +69,7 @@ class Pinterest_Helper(object):
         return tmp2        
         
     
-    def runme(self,url, threshold = 500, persistence = 120):
+    def runme(self,url, threshold = 500, persistence = 120, debug = False):
         final_results = []
         previmages = []
         tries = 0
@@ -87,20 +87,21 @@ class Pinterest_Helper(object):
                         return final_results
                     for i in images:
                         src = i.get_attribute("src")
+                        if debug == True:
+                            print(src)
                         if src:
-                            #if string.find(src,"/236x/") != -1:
                             if src.find("/236x/") != -1:
-                                #src = string.replace(src,"/236x/","/736x/")
                                 src = src.replace("/236x/","/736x/")
                                 results.append(u_to_s(src))
                     previmages = copy.copy(images)
                     final_results = list(set(final_results + results))
                     dummy = self.browser.find_element_by_tag_name('a')
                     dummy.send_keys(Keys.PAGE_DOWN)
-                    # images[0].send_keys(Keys.PAGE_DOWN)
                     randdelay(0,1)
                     threshold -= 1
                 except (StaleElementReferenceException):
+                    if debug == True:
+                        print("StaleElementReferenceException")
                     threshold -= 1
         except (socket.error, socket.timeout):
             pass
